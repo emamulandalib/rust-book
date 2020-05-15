2c10cd37ee08e77842c3e840977be08297be6a1d
Emamul Andalib <emamul.andalib@gmail.com>
2020-05-15 20:57:33 +0600

    - Up to listing 4.7

---
+++ chapter_4_3_first_word/src/main.rs
@@ -0,0 +1,16 @@
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+}
+
+fn main() {
+    let pos = first_word(&String::from("Hello World!"));
+    println!("{}", pos)
+}
