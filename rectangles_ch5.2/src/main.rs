457a900a929405110e37587f7ed5e0707d543496
Emamul Andalib <emamul.andalib@gmail.com>
2020-05-17 22:08:41 +0600

    - finished at listing 5-8

--- rectangles_ch5.2/src/main.rs
+++ rectangles_ch5.2/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let w = 30;
+    let h = 50;
+
+    println!("This area of the rectangle is {} square pixels.", area(w, h))
+}
+
+fn area(w: u32, h: u32) -> u32 {
+    w * h
 }
