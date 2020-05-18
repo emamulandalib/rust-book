8bb9509dfd8bdd75d7ad794b26547151a96e1850
Emamul Andalib <emamul.andalib@gmail.com>
2020-05-18 16:37:02 +0600

    - finished till 5-13

--- rectangles_ch5.2/src/main.rs
+++ rectangles_ch5.2/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32
@@ -9,7 +10,8 @@ fn main() {
         height: 50
     };
 
-    println!("This area of the rectangle is {} square pixels.", area(&rect))
+    println!("{:#?}", rect)
+    // println!("This area of the rectangle is {} square pixels.", area(&rect))
 }
 
 fn area(rect: &Rectangle) -> u32 {
---
+++ rectangles_ch5.3/src/main.rs
@@ -0,0 +1,21 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }    
+}
+
+fn main() {
+    let rect = Rectangle{
+        width: 30,
+        height: 50
+    };
+
+    // println!("{:#?}", rect)
+    println!("This area of the rectangle is {} square pixels.", rect.area())
+}
