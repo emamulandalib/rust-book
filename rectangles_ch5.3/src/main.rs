4202462cbdf876e0551730cc214dd1d5d4f97898
Emamul Andalib <emamul.andalib@gmail.com>
2020-05-18 16:40:26 +0600

    - finished till 5-14

--- rectangles_ch5.3/src/main.rs
+++ rectangles_ch5.3/src/main.rs
@@ -8,6 +8,14 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }    
+
+    fn can_hold(&self, rect: &Rectangle) -> bool {
+        if self.height > rect.height && self.width > rect.width {
+            return true
+        }
+
+        false
+    }
 }
 
 fn main() {
@@ -16,6 +24,21 @@ fn main() {
         height: 50
     };
 
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
     // println!("{:#?}", rect)
-    println!("This area of the rectangle is {} square pixels.", rect.area())
+    println!("This area of the rectangle is {} square pixels.", rect.area());
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
